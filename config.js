c21f427cb9098eddee13e1adbd15ebcb9310c3a2
duongvanthanh2011 <58882839+duongvanthanh2011@users.noreply.github.com>
2022-10-20 02:23:11 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,23 @@
+const CONFIG = {
+    introTitle: 'Em bé à!',
+    introDesc: `Hôm nay là 1 ngày đặc biệt.
+    Một phép tính đơn giản 20/10 đúng khum nào :3.
+    Nhưng mà trước hết anh có 1 câu hỏi 🥺??`,
+    btnIntro: 'Ấn tiếp ik :3',
+    title: 'Em bé con yêu anh nhiều không 😞 ',
+    desc: 'Gét anh hong 🥺?? ',
+    btnYes: 'Vẫn cứ là iu anh :3 💗💗',
+    btnNo: 'EM GÉT ANH LẮM 🤬',
+    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
+    btnReply: 'Gửi cho anh <3',
+    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    mess: `Anh biết mà 🥰. Anh cx Yêu em nhiều nhiều 😘😘. 
+    Hôm nay là ngày 20/10 chú vo yêu của anh sẽ luôn
+    mạnh khỏe, vui vẻ, luôn gặp được may mắn và hạnh phúc nè và xinh đẹp hơn :3. 
+    Săp tới thì giảm cân thành công và dễ dàng 💪💪. 
+    Và luôn yêu anh vo anh nhé 💋💋😊😊`,
+    messDesc: `Tối nay 7h anh qua đón nhé công chúa.\n
+    Chúc vo iu sẽ trở thành người phụ nữ của anh :3`,
+    btnAccept: 'Okiiiii lun <3',
+    messLink: 'https://www.facebook.com/messages/t/100009265167681'
+}
